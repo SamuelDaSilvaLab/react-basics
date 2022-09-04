94d4b0a0ed9a35aebe6d06edcae1e3a2aa73e601
SamuelDaSilvaLab <89095302+SamuelDaSilvaLab@users.noreply.github.com>
2022-09-04 15:08:02 -0300

    comps end props

--- src/pages/Basics.jsx
+++ src/pages/Basics.jsx
@@ -1,7 +1,9 @@
 // import SayHello from "../topics/001-SayHello";
 // import JSXBasics from "../topics/002-JSXBasics";
 // import { FuncComponent, ES6Component } from "../topics/004-ComponentsAndProps";
-import RendElements from "../topics/003-RendElements";
+// import RendElements from "../topics/003-RendElements";
+
+import CompAndProps from "../topics/004-ComponentsAndProps";
 
 export default function Basics() {
   return (
@@ -10,7 +12,8 @@ export default function Basics() {
       <JSXBasics />
       <FuncComponent name="Functional Component" />
       <ES6Component text="ES6 Component" /> */}
-      <RendElements />
+      {/* <RendElements /> */}
+      <CompAndProps />
     </>
   );
 }
--- src/topics/004-ComponentsAndProps.jsx
+++ src/topics/004-ComponentsAndProps.jsx
@@ -10,4 +10,12 @@ class ES6Component extends React.Component {
   }
 }
 
-export { FuncComponent, ES6Component };
+export default function CompAndProps() {
+  return (
+    <>
+      <h1>Components and Props</h1>
+      <FuncComponent name="Samuel da Silva" />
+      <ES6Component text="ES6 Component" />
+    </>
+  );
+}
